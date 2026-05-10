c6b7ec1a1bfd49083ec8510a195a2dd03812e449
Marco Berzborn <m.berzborn@tue.nl>
2026-05-10 22:34:05 +0200

    Remove HelperFunctions and simulation_method_interface from common folder

--- common/simulation_method_interface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from pathlib import Path
-
-class SimulationMethod(ABC):
-    @abstractmethod
-    def run_simulation(self, json_file_path: str | Path):
-        """ Runs the simulation given a json file. """
-        pass
-    
